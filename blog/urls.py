6da570484c3a4224d5370c8f3ded182125cdb850
steve <stevenmeare@gmail.com>
2024-05-01 20:26:00 +0100

    Docstring added to blog/urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,15 @@ from django.views.generic import ListView, DetailView
 from django.urls import path
 from .models import Post
 
+"""
+    The URL config for the blog app. This file is responsible for mapping URLs to views.
+    The default view for the blog app is the index view, which shows a list of all the posts.
+    The index view is implemented as a ListView, that displays a list of objects.
+    It is configured to display the 25 most recent posts, ordered by date.
+    The detail view displays a single post, with the primary key specified in the URL.
+
+"""
+
 app_name='blog'
 urlpatterns = [
     # Show a list of all "Post" objects
