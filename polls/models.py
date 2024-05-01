e88a479e7f8c3d181b02fc68963495ba979ec710
steve <stevenmeare@gmail.com>
2024-05-01 21:47:20 +0100

    Added docstrings to poll app scripts

--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,20 @@ from django.db import models
 
 # Define the question class
 class Question(models.Model):
+    '''
+        This model defines a question. 
+        The question has a one-to-many relationship with the choice model.
+
+        :param question_text: The text of the question.
+        :type question_text: str
+
+        :param pub_date: The date the question was published.
+        :type pub_date: datetime
+
+        :return: The text of the question.
+        :rtype: str
+        
+    '''
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
@@ -12,6 +26,23 @@ class Question(models.Model):
 
 # Define the choice class
 class Choice(models.Model):
+    '''
+        This model defines a choice. 
+        The choice has a many-to-one relationship with the question model.
+
+        :param question: The question the choice is associated with.
+        :type question: Question
+
+        :param choice_text: The text of the choice.
+        :type choice_text: str
+
+        :param votes: The number of votes for the choice.
+        :type votes: int
+
+        :return: The text of the choice.
+        :rtype: str
+        
+    '''
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
--- polls/views.py
+++ polls/views.py
@@ -7,22 +7,72 @@ from .models import Question, Choice
 
 # Shows a list of polls
 def index(request):
+    '''
+    This function returns a list of the latest 5 questions.
+    The questions are ordered by publication date.
+
+    :param request: The request object.
+    :type request: HttpRequest
+
+    :return: A list of the latest 5 questions.
+    :rtype: HttpResponse
+
+    '''
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, "polls/poll.html", context)
 
 # Shows question template
 def detail(request, question_id):
+    '''
+    This function returns a question template.
+    
+    :param request: The request object.
+    :type request: HttpRequest
+    
+    :param question_id: The id of the question.
+    :type question_id: int
+
+    :return: A question template.
+    :rtype: HttpResponse
+    '''
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 # Shows poll result
 def results(request, question_id):
+    '''
+    This function returns the results of a poll.
+
+    :param request: The request object.
+    :type request: HttpRequest
+
+    :param question_id: The id of the question.
+    :type question_id: int
+
+    :return: The results of a poll.
+    :rtype: HttpResponse
+    '''
+
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
 # Posts form choice, or returns error
 def vote(request, question_id):
+    '''
+    This function allows users to vote on a poll.
+    
+    :param request: The request object.
+    :type request: HttpRequest
+    
+    :param question_id: The id of the question.
+    :type question_id: int
+
+    :exception: Choice.DoesNotExist: If the choice does not exist.
+    
+    :return: The results of a poll.
+    :rtype: HttpResponseRedirect
+    '''
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(
